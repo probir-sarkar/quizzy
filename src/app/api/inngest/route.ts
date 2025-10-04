2b9ec97de61af106749237b2036b4ee5c0e41fb2
Probir Sarkar <probirsarkar855@gmail.com>
2025-10-04 13:10:41 +0530

    telegram bot added

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,9 +2,10 @@ import { serve } from "inngest/next";
 import { helloWorld } from "@/inngest/functions";
 import { inngest } from "@/inngest/client";
 import { generateQuizFn } from "@/inngest/geretare-quiz";
+import { socialMediaShare } from "@/inngest/social-media-share/social-media-share.inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld, generateQuizFn],
+  functions: [helloWorld, generateQuizFn, socialMediaShare],
 });
