27211f3260ef02c0e5c40f981725aef72f6a5559
Probir Sarkar <probirsarkar855@gmail.com>
2025-10-07 21:08:37 +0530

    build issue fix

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { Sparkles, ChevronLeft } from "lucide-react";
 import prisma from "@/lib/prisma";
+export const dynamic = "force-dynamic";
 
 export default async function CategoriesPage() {
   const categories = await prisma.category.findMany({
