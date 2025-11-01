b7a79883ae5cfa73da1f0547678136f49da27ede
Probir Sarkar <probirsarkar855@gmail.com>
2025-11-01 15:55:02 +0530

    phase 1 complete of Imge generation

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,78 @@
 export const BASE_URL = "https://quizzone.club";
 export const TELEGRAM_CHANNEL_URL = "https://t.me/quizzone_club";
+
+
+export const zodiacSigns = [
+  {
+    name: "ARIES",
+    symbol: "♈️",
+    dates: "Mar 21 - Apr 19",
+    element: { name: "Fire", icon: "🔥" }
+  },
+  {
+    name: "TAURUS",
+    symbol: "♉️",
+    dates: "Apr 20 - May 20",
+    element: { name: "Earth", icon: "🌍" }
+  },
+  {
+    name: "GEMINI",
+    symbol: "♊️",
+    dates: "May 21 - Jun 20",
+    element: { name: "Air", icon: "🌬️" }
+  },
+  {
+    name: "CANCER",
+    symbol: "♋️",
+    dates: "Jun 21 - Jul 22",
+    element: { name: "Water", icon: "💧" }
+  },
+  {
+    name: "LEO",
+    symbol: "♌️",
+    dates: "Jul 23 - Aug 22",
+    element: { name: "Fire", icon: "🔥" }
+  },
+  {
+    name: "VIRGO",
+    symbol: "♍️",
+    dates: "Aug 23 - Sep 22",
+    element: { name: "Earth", icon: "🌍" }
+  },
+  {
+    name: "LIBRA",
+    symbol: "♎️",
+    dates: "Sep 23 - Oct 22",
+    element: { name: "Air", icon: "🌬️" }
+  },
+  {
+    name: "SCORPIO",
+    symbol: "♏️",
+    dates: "Oct 23 - Nov 21",
+    element: { name: "Water", icon: "💧" }
+  },
+  {
+    name: "SAGITTARIUS",
+    symbol: "♐️",
+    dates: "Nov 22 - Dec 21",
+    element: { name: "Fire", icon: "🔥" }
+  },
+  {
+    name: "CAPRICORN",
+    symbol: "♑️",
+    dates: "Dec 22 - Jan 19",
+    element: { name: "Earth", icon: "🌍" }
+  },
+  {
+    name: "AQUARIUS",
+    symbol: "♒️",
+    dates: "Jan 20 - Feb 18",
+    element: { name: "Air", icon: "🌬️" }
+  },
+  {
+    name: "PISCES",
+    symbol: "♓️",
+    dates: "Feb 19 - Mar 20",
+    element: { name: "Water", icon: "💧" }
+  }
+];
