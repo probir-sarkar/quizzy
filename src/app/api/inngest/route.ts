c43fe2ea8c1ff6c10c393fb7d057a1d6bd5001a5
Probir Sarkar <probirsarkar855@gmail.com>
2026-01-25 11:39:28 +0530

    code optimize

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
-import { generateQuizFn } from "@/inngest/geretare-quiz";
- // this API route is dynamic
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [generateQuizFn]
-});
